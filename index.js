cfed0306c72a1bab7426bb24ae139f231db8097d
TungTran <tranhoangtung11@gmail.com>
2023-03-04 11:42:12 -0800

    Added conditions when user input nothing

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 let myLeadNames = []
 let myLeadCompany = []
 let myLeadLinks = []
+let myLeadPosition = 0
 
 const nameInputEl = document.getElementById("name-input-el")
 const companyInputEl = document.getElementById("company-input-el")
 const linkInputEl = document.getElementById("link-input-el")
 
+
 const ulElName = document.getElementById("ul-el-name")
 const ulElCompany = document.getElementById("ul-el-company")
 const ulElLink = document.getElementById("ul-el-link")
-
+const ulElPosition = document.getElementById("ul-el-position")
 
 const inputBtn = document.getElementById("input-btn")
 const tabBtn = document.getElementById("tab-btn")
@@ -26,16 +28,24 @@ inputBtn.addEventListener("click", function(){
     companyInputEl.value = ""
     linkInputEl.value = ""
 
+    renderPosition()
     renderName(myLeadNames)
     renderCompany(myLeadCompany)
     renderLink(myLeadLinks)
 })
+function renderPosition(){
+    myLeadPosition ++
+    ulElPosition.innerHTML += `
+            <p id="lead-position">${myLeadPosition}</p>
+    `
+}
+
 
 function renderName(name){
     let listItemsName = ""
     for (let i = 0; i < name.length; i++) {
         listItemsName += `
-                <p>${name[i]}</p>
+                <p id="lead-name">${name[i]}</p>
         ` 
     }
     ulElName.innerHTML = listItemsName
@@ -45,7 +55,7 @@ function renderCompany(company){
     let listItemsCompany = ""
     for (let i = 0; i < company.length; i++) {
         listItemsCompany += `
-                <p>${company[i]}</p>
+                <p id="lead-company">${company[i]}</p>
         ` 
     }
     ulElCompany.innerHTML = listItemsCompany
@@ -57,7 +67,7 @@ function renderLink(links){
     let listItemsLink = ""
     for (let i = 0; i < links.length; i++) {
         listItemsLink += `
-                <a target='_blank' href='${links[i]}'>
+                <a target='_blank' href='${links[i]} id="lead-link'>
                     ${links[i]}
                 </a>
         ` 
