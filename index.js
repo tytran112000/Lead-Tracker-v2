bf1e1b5408810d763d18f4f2b32a28b3ad4f8803
TungTran <tranhoangtung11@gmail.com>
2023-03-06 18:30:13 -0800

    Save everything to localStorage+change li to table

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let myLeadNames = []
 let myLeadCompany = []
 let myLeadLinks = []
 let myLeadPosition = 0
+let myLeadPositionArray = []
 
 const nameInputEl = document.getElementById("name-input-el")
 const companyInputEl = document.getElementById("company-input-el")
 const linkInputEl = document.getElementById("link-input-el")
-
 
 const ulElName = document.getElementById("ul-el-name")
 const ulElCompany = document.getElementById("ul-el-company")
@@ -19,42 +19,84 @@ const tabBtn = document.getElementById("tab-btn")
 const exportBtn = document.getElementById("export-btn")
 const deleteBtn = document.getElementById("delete-btn")
 
+const nameFromLocalStorage = JSON.parse(localStorage.getItem("myLeadNames"))
+const companyFromLocalStorage = JSON.parse(localStorage.getItem("myLeadCompany"))
+const linkFromLocalStorage = JSON.parse(localStorage.getItem("myLeadLinks"))
+const positionFromLocalStorage = JSON.parse(localStorage.getItem("myLeadPositionArray"))
+
+if (nameFromLocalStorage) {
+    myLeadNames = nameFromLocalStorage
+    renderName(myLeadNames)
+}
+
+if (companyFromLocalStorage) {
+    myLeadCompany = companyFromLocalStorage
+    renderCompany(myLeadCompany)
+}
+
+if (linkFromLocalStorage) {
+    myLeadLinks = linkFromLocalStorage
+    renderLink(myLeadLinks)
+}
+if (positionFromLocalStorage){
+    myLeadPositionArray = positionFromLocalStorage
+    renderPosition(myLeadPositionArray)
+}
+
+
+
+// Output input information when clicked input button
 inputBtn.addEventListener("click", function(){
     if(nameInputEl.value){
         myLeadNames.push(nameInputEl.value)
         nameInputEl.value = ""
+        localStorage.setItem("myLeadNames", JSON.stringify(myLeadNames))
         renderName(myLeadNames)
     } else if (nameInputEl.value != true){
         myLeadNames.push("N/A")
+        localStorage.setItem("myLeadNames", JSON.stringify(myLeadNames))
         renderName(myLeadNames)
     }
 
     if (companyInputEl.value){
         myLeadCompany.push(companyInputEl.value)
         companyInputEl.value = ""
+        localStorage.setItem("myLeadCompany", JSON.stringify(myLeadCompany))
         renderCompany(myLeadCompany)
     } else if (companyInputEl.value != true){
         myLeadCompany.push("N/A")
+        localStorage.setItem("myLeadCompany", JSON.stringify(myLeadCompany))
         renderCompany(myLeadCompany)
     }
 
     if (linkInputEl.value){
+        myLeadPositionArray.push(myLeadPosition)
+        localStorage.setItem("myLeadPositionArray", JSON.stringify(myLeadPositionArray))
+        renderPosition(myLeadPositionArray)
+
         myLeadLinks.push(linkInputEl.value)
         linkInputEl.value = ""
+        localStorage.setItem("myLeadLinks", JSON.stringify(myLeadLinks))
         renderLink(myLeadLinks)
     } else if (linkInputEl.value != true){
         myLeadLinks.push("N/A")
+        localStorage.setItem("myLeadLinks", JSON.stringify(myLeadLinks))
         renderLink(myLeadLinks)
     }
-    renderPosition()
+    
+    
 })
 
-
-function renderPosition(){
+// render position, name, company
+function renderPosition(position){
     myLeadPosition ++
-    ulElPosition.innerHTML += `
-            <p id="lead-position">${myLeadPosition}</p>
-    `
+    let listItemsPosition = ""
+    for (let i = 0; i < position.length; i ++){
+        listItemsPosition += `
+                <p id="child">${position[i]+1}</i>
+        `
+    }
+    ulElPosition.innerHTML = listItemsPosition
 }
 
 
@@ -62,7 +104,7 @@ function renderName(name){
     let listItemsName = ""
     for (let i = 0; i < name.length; i++) {
         listItemsName += `
-                <p id="lead-name">${name[i]}</p>
+                <p id="child-one">${name[i]}</p>
         ` 
     }
     ulElName.innerHTML = listItemsName
@@ -72,7 +114,7 @@ function renderCompany(company){
     let listItemsCompany = ""
     for (let i = 0; i < company.length; i++) {
         listItemsCompany += `
-                <p id="lead-company">${company[i]}</p>
+                <p id="child-two">${company[i]}</p>
         ` 
     }
     ulElCompany.innerHTML = listItemsCompany
@@ -84,10 +126,47 @@ function renderLink(links){
     let listItemsLink = ""
     for (let i = 0; i < links.length; i++) {
         listItemsLink += `
-                <a target='_blank' href='${links[i]} id="lead-link'>
+                <a target='_blank' href='${links[i]}' id="child-three">
                     ${links[i]}
                 </a>
         ` 
     }
     ulElLink.innerHTML = listItemsLink
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeadNames = []
+    myLeadCompany = []
+    myLeadLinks = []
+    myLeadPositionArray = []
+    myLeadPosition = -1
+    renderName(myLeadNames)
+    renderCompany(myLeadCompany)
+    renderLink(myLeadLinks)
+    renderPosition(myLeadPositionArray)
+})
+
+document.getElementById("export-btn").addEventListener("click", exportListToCSV);
+
+function exportListToCSV() {
+    var listItems = document.getElementsByTagName("td");
+    var listArray = Array.from(listItems).map(function (item) {
+        return  item.textContent;
+    });
+    console.log(listArray)
+    var csvString = listArray.join(" ,");
+  
+    function downloadCSV(csv) {
+      var filename = "list.csv";
+      var link = document.createElement("a");
+      link.style.display = "none";
+      link.setAttribute("target", "_blank");
+      link.setAttribute("href", "data:text/csv;charset=utf-8," + encodeURIComponent(csv));
+      link.setAttribute("download", filename);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+    downloadCSV(csvString);
+  }
